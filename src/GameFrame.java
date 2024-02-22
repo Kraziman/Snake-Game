6d26444460c31cae03f91bd0216abea425312fef
Kraziman <notkraziman@gmail.com>
2024-02-22 11:07:50 +0200

    Improve Snake Game: - Allow the snake to emerge from the opposite side when hitting a wall - Prevent the snake from moving backward due to rapid navigation key presses - Scale the eyes of the snake in proportion to the UNIT_SIZE

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,6 +5,7 @@ public class GameFrame extends JFrame {
     GameFrame(){
         this.add(new GamePanel());
         this.setTitle("Snake");
+        //TODO: Fix the Icon not showing when game is ran through cmd
         this.setIconImage(new ImageIcon("Icon/Icon.png").getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,11 +12,15 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int DELAY = 75;
     final int[] x = new int[GAME_UNITS];
     final int[] y = new int[GAME_UNITS];
+
+    int snakeEyePosOne = (int)(((int)(UNIT_SIZE / 20)) * 3);
+    int snakeEyePosTwo = (int)(((int)(UNIT_SIZE / 4)) * 3);
     int bodyParts = 6;
     int applesEaten;
     int appleX;
     int appleY;
     Direction direction = Direction.Right;
+    int distanceSinceLastDirectionChange = 0;
     boolean running = false;
     Timer timer;
     Random random;
@@ -47,52 +51,65 @@ public class GamePanel extends JPanel implements ActionListener {
         draw(g);
     }
 
+    public void drawSnakeHead(Graphics g, int i){
+        g.setColor(Color.GREEN);
+        g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+
+        // Eyes of the snake
+        g.setColor(Color.BLACK);
+        switch (direction) {
+            case Left:
+                g.fillRect(x[0] + snakeEyePosOne, y[0] + snakeEyePosOne, snakeEyePosOne, snakeEyePosOne);
+                g.fillRect(x[0] + snakeEyePosOne, y[0] + snakeEyePosTwo, snakeEyePosOne, snakeEyePosOne);
+                break;
+            case Right:
+                g.fillRect(x[0] + snakeEyePosTwo, y[0] + snakeEyePosOne, snakeEyePosOne, snakeEyePosOne);
+                g.fillRect(x[0] + snakeEyePosTwo, y[0] + snakeEyePosTwo, snakeEyePosOne, snakeEyePosOne);
+                break;
+            case Up:
+                g.fillRect(x[0] + snakeEyePosOne, y[0] + snakeEyePosOne, snakeEyePosOne, snakeEyePosOne);
+                g.fillRect(x[0] + snakeEyePosTwo, y[0] + snakeEyePosOne, snakeEyePosOne, snakeEyePosOne);
+                break;
+            case Down:
+                g.fillRect(x[0] + snakeEyePosOne, y[0] + snakeEyePosTwo, snakeEyePosOne, snakeEyePosOne);
+                g.fillRect(x[0] + snakeEyePosTwo, y[0] + snakeEyePosTwo, snakeEyePosOne, snakeEyePosOne);
+                break;
+        }
+    }
+
+    public void drawSnake(Graphics g){
+        g.setColor(Color.RED);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                drawSnakeHead(g, i);
+            } else {
+                if(i%2 == 0){
+                    g.setColor(Color.GREEN);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+                else {
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+            drawSnakeHead(g, 0);
+        }
+    }
+
     public void draw(Graphics g){
 
         if(running) {
-            g.setColor(Color.RED);
-            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-
-            for (int i = 0; i < bodyParts; i++) {
-                if (i == 0) {
-                    g.setColor(Color.GREEN);
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-
-                    // Eyes of the snake
-                    g.setColor(Color.BLACK);
-                    switch (direction) {
-                        case Left:
-                            g.fillRect(x[0] + 3, y[0] + 3, 3, 3);
-                            g.fillRect(x[0] + 3, y[0] + 15, 3, 3);
-                            break;
-                        case Right:
-                            g.fillRect(x[0] + 15, y[0] + 3, 3, 3);
-                            g.fillRect(x[0] + 15, y[0] + 15, 3, 3);
-                            break;
-                        case Up:
-                            g.fillRect(x[0] + 3, y[0] + 3, 3, 3);
-                            g.fillRect(x[0] + 15, y[0] + 3, 3, 3);
-                            break;
-                        case Down:
-                            g.fillRect(x[0] + 3, y[0] + 15, 3, 3);
-                            g.fillRect(x[0] + 15, y[0] + 15, 3, 3);
-                            break;
-                    }
-                } else {
-                    if(i%2 == 0){
-                        g.setColor(Color.GREEN);
-                        g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-                    }
-                    else {
-                        g.setColor(new Color(45, 180, 0));
-                        g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-                    }
-                }
-            }
+            //draws the snake
+            drawSnake(g);
             //Scoreboard text
             scoreboard(g);
+            //Increases the value of distanceSinceLastDirectionChange every time the snake is drawn
+            distanceSinceLastDirectionChange++;
         }
         else {
+            //Draws 'Game Over!' screen
             gameOver(g);
         }
     }
@@ -154,22 +171,22 @@ public class GamePanel extends JPanel implements ActionListener {
 
         //check if head touches left border
         if (x[0] < 0){
-            x[0] = SCREEN_WIDTH;
+            x[0] = SCREEN_WIDTH - UNIT_SIZE;
         }
 
         //check if head touches right border
-        if (x[0] > SCREEN_WIDTH){
+        if (x[0] > SCREEN_WIDTH - UNIT_SIZE){
             x[0] = 0;
         }
 
         //check if head touches top border
         if (y[0] < 0){
             /*running = false;*/
-            y[0] = SCREEN_HEIGHT;
+            y[0] = SCREEN_HEIGHT - UNIT_SIZE;
         }
 
         //check if head touches bottom border
-        if (y[0] > SCREEN_HEIGHT){
+        if (y[0] > SCREEN_HEIGHT - UNIT_SIZE){
             y[0] = 0;
         }
 
@@ -179,6 +196,9 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void gameOver(Graphics g){
+        //Draw last frame of the game
+        drawSnake(g);
+
         //Scoreboard text
         scoreboard(g);
 
@@ -202,28 +222,34 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public class MyKeyAdapter extends KeyAdapter{
         @Override
-        public void keyPressed(KeyEvent e){
-            switch(e.getKeyCode()){
-                case KeyEvent.VK_LEFT:
-                    if(direction != Direction.Right){
-                        direction = Direction.Left;
-                    }
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    if(direction != Direction.Left){
-                        direction = Direction.Right;
-                    }
-                    break;
-                case KeyEvent.VK_UP:
-                    if(direction != Direction.Down){
-                        direction = Direction.Up;
-                    }
-                    break;
-                case KeyEvent.VK_DOWN:
-                    if(direction != Direction.Up){
-                        direction = Direction.Down;
-                    }
-                    break;
+        public void keyPressed(KeyEvent e) {
+            if (distanceSinceLastDirectionChange >= 1) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        if (direction != Direction.Right) {
+                            direction = Direction.Left;
+                            distanceSinceLastDirectionChange = 0;
+                        }
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        if (direction != Direction.Left) {
+                            direction = Direction.Right;
+                            distanceSinceLastDirectionChange = 0;
+                        }
+                        break;
+                    case KeyEvent.VK_UP:
+                        if (direction != Direction.Down) {
+                            direction = Direction.Up;
+                            distanceSinceLastDirectionChange = 0;
+                        }
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        if (direction != Direction.Up) {
+                            direction = Direction.Down;
+                            distanceSinceLastDirectionChange = 0;
+                        }
+                        break;
+                }
             }
         }
     }
